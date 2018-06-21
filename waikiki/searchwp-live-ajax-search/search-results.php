7c013a6249a97fb0b5de4b48d4ab1642fd650a20
cnbjornsen <38555816+cnbjornsen@users.noreply.github.com>
2018-06-21 11:11:14 +0200

    Changes to live search CSS + functions

--- waikiki/searchwp-live-ajax-search/search-results.php
+++ waikiki/searchwp-live-ajax-search/search-results.php
@@ -1,7 +1,7 @@
 <?php /**
 * This template adds thumbnails to your SearchWP Live Ajax Search results.
-* 
-* Create a folder called searchwp-live-ajax-search in your child theme folder and copy this file in there. 
+*
+* Create a folder called searchwp-live-ajax-search in your child theme folder and copy this file in there.
 * In order for this to work you require the SearchWP and SearchWP Live Ajax Search plugins installed and active.
 *
 */
@@ -12,12 +12,12 @@
 	<?php while ( have_posts() ) : the_post(); ?>
 		<?php $post_type = get_post_type_object( get_post_type() ); ?>
 		<div class="searchwp-live-search-result" style="white-space: nowrap;">
-		<span style="display: inline-block; margin-left: 5px;"><?php echo( get_the_post_thumbnail( $post->ID, array( 50, 50) ) ) ?></span>
-		<span style="display: inline-block;">
+		<div style="display: inline-block;"><?php echo( get_the_post_thumbnail( $post->ID, array( 75, 75) ) ) ?></div>
+		<div style="display: inline-block;">
 			<p><a href="<?php echo esc_url( get_permalink() ); ?>">
 				<?php the_title(); ?> &raquo;
 			</a></p>
-		</span>
+		</div>
 		</div>
 	<?php endwhile; ?>
 <?php else : ?>
