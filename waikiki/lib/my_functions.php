cd58f2b216c5cf0b08fab5bd6d3cd9155e8957ff
cnbjornsen <38555816+cnbjornsen@users.noreply.github.com>
2018-06-21 13:18:54 +0200

    Pricing test #2
    Added function to remove price range and add "from" price instead

--- waikiki/lib/my_functions.php
+++ waikiki/lib/my_functions.php
@@ -69,27 +69,27 @@ function jk_dequeue_styles( $enqueue_styles ) {
  * @param obj $product
  * @return str
  */
-// function iconic_variable_price_format( $price, $product ) {
-//
-//     $prefix = sprintf('%s: ', __('Fra', 'iconic'));
-//
-//     $min_price_regular = $product->get_variation_regular_price( 'min', true );
-//     $min_price_sale    = $product->get_variation_sale_price( 'min', true );
-//     $max_price = $product->get_variation_price( 'max', true );
-//     $min_price = $product->get_variation_price( 'min', true );
-//
-//     $price = ( $min_price_sale == $min_price_regular ) ?
-//         wc_price( $min_price_regular ) :
-//         '<del>' . wc_price( $min_price_regular ) . '</del>' . '<ins>' . wc_price( $min_price_sale ) . '</ins>';
-//
-//     return ( $min_price == $max_price ) ?
-//         $price :
-//         sprintf('%s%s', $prefix, $price);
-//
-// }
-//
-// add_filter( 'woocommerce_variable_sale_price_html', 'iconic_variable_price_format', 10, 2 );
-// add_filter( 'woocommerce_variable_price_html', 'iconic_variable_price_format', 10, 2 );
+function iconic_variable_price_format( $price, $product ) {
+
+    $prefix = sprintf('%s: ', __('Fra', 'iconic'));
+
+    $min_price_regular = $product->get_variation_regular_price( 'min', true );
+    $min_price_sale    = $product->get_variation_sale_price( 'min', true );
+    $max_price = $product->get_variation_price( 'max', true );
+    $min_price = $product->get_variation_price( 'min', true );
+
+    $price = ( $min_price_sale == $min_price_regular ) ?
+        wc_price( $min_price_regular ) :
+        '<del>' . wc_price( $min_price_regular ) . '</del>' . '<ins>' . wc_price( $min_price_sale ) . '</ins>';
+
+    return ( $min_price == $max_price ) ?
+        $price :
+        sprintf('%s%s', $prefix, $price);
+
+}
+
+add_filter( 'woocommerce_variable_sale_price_html', 'iconic_variable_price_format', 10, 2 );
+add_filter( 'woocommerce_variable_price_html', 'iconic_variable_price_format', 10, 2 );
 
 
 /**
